dfa8333fe311a920583e1c9344da8380416c62e8
Elin Filipowicz <elin.filipowicz@nordnet.se>
2018-04-27 08:58:25 +0200

    Fixed pull request comments.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,14 +8,6 @@ const mainMenu = [
     { text: 'Submit', url: 'https://news.ycombinator.com/submit' },
 ];
 
-const numbers = [1 ,2 ,3];
-const double = a => a * 2;
-console.log(numbers.map(double));
-
-const first = mainMenu[0];
-console.log(first.text);
-console.log(first.url);
-
 export const Header = () => (
     <div className='header'>
         <span className='header__logo'>
