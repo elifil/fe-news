3b47afdddd54dd22dc1eaa866c3dac6ed020f9f2
Elin Filipowicz <elin.filipowicz@nordnet.se>
2018-05-16 13:00:04 +0200

    Modified PageNewsList to handle promises.

--- src/components/page-news-list/index.js
+++ src/components/page-news-list/index.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NewsItemList } from "../";
 import { api } from '../../utils';
 
-export const PageNewsList = () => (
-    <NewsItemList ids={api.getItemsIds()}/>
-)
+const isArraysEqual = (arr1, arr2) => arr1.toString() === arr2.toString();
+
+export class PageNewsList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: undefined,
+        }
+    }
+    componentDidMount() {
+        api.getItemsIds()
+            .then(data => { this.setState({ data }) })
+            .catch(err => { console.error(err) });
+    }
+    shouldComponetUpdate(nextProps, nextState) {
+        // TODO: access current this.state and this.props
+        // use isArraysEqual to check list of ids for `/`
+    }
+    render() {
+        const { data } = this.state;
+        if (!data) {
+            return <div>Loading…</div>
+        }
+        return (
+            <div>
+                <NewsItemList ids={data}/>
+            </div>
+        )
+    }
+}
