5e5b809e11bf517fa1754f04f23fcf5eb47b5eba
Elin Filipowicz <elin.filipowicz@nordnet.se>
2018-05-30 13:10:21 +0200

    Added redux once more (and deleted the old stuff).

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { valueReducer } from './redux/reducers';
-import { updateValueTo10 } from './redux/actions';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-const store = createStore(valueReducer);
-store.dispatch(updateValueTo10());
--- src/redux/actions.js
+++
@@ -1,5 +0,0 @@
-export const UPDATE_VALUE_TO_TEN = 'UPDATE_VALUE_TO_TEN';
-
-export const updateValueTo10 = () => {
-    return { type: UPDATE_VALUE_TO_TEN }
-};
