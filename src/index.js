3abd81132175b31532529a208668b42440385952
Elin Filipowicz <elin.filipowicz@nordnet.se>
2018-05-30 15:54:58 +0200

    Applied redux-thunk.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-import { createStore } from 'redux'
-import { rootReducer } from './reducers/';
-import { Provider } from 'react-redux';
+import { rootReducer } from "./reducers";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from 'redux-thunk';
+import { Provider } from "react-redux";
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 ReactDOM.render(
