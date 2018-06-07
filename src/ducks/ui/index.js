33777c6032b24376f9a8f736485cb5d911cb9c58
Elin Filipowicz <elin.filipowicz@nordnet.se>
2018-06-07 11:50:39 +0200

    Added ducks.

---
+++ src/ducks/ui/index.js
@@ -0,0 +1,62 @@
+import PropTypes from 'prop-types';
+
+export const ns = 'ui-duck';
+
+export const shape = {
+  itemsToShow: PropTypes.number.isRequired,
+  isDarkTheme: PropTypes.bool.isRequired,
+};
+
+export const defaultState = {
+  itemsToShow: 10,
+  isDarkTheme: false,
+};
+
+const root = state => state[ns];
+
+export const selectors = {
+  root,
+  itemsToShow: state => root(state).itemsToShow,
+  isDarkTheme: state => root(state).isDarkTheme,
+}
+
+export const types = {
+  updateItemsToShow: `${ns} / UPDATE_ITEMS_TO_SHOW`,
+  toggleTheme: `${ns} / TOGGLE_THEME`,
+
+}
+
+export const updateItemsToShow = value => ({
+  type: types.updateItemsToShow,
+  payload: value,
+});
+
+const toggleTheme = () => ({
+  type: types.toggleTheme,
+});
+
+
+export const actions = {
+  toggleTheme,
+  updateItemsToShow,
+}
+
+export const rawReducer = (state=defaultState, action) => {
+  switch (action.type) {
+    case types.updateItemsToShow:
+      return {
+      ...state,
+      itemsToShow: action.payloadDescription
+    };
+    case types.toggleTheme:
+      return {
+        ...state,
+        isDarkTheme: !state.isDarkTheme
+      }
+    default: return state;
+  }
+}
+
+export const reducer = {
+  [ns]: rawReducer
+}
